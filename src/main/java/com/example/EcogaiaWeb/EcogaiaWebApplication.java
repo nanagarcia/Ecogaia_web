457368d659ddd20d972c6e7620740b4dd69a7561
Valeria <dannasuarez786@gmail.com>
2023-04-27 11:15:06 -0500

    base

---
+++ src/main/java/com/example/EcogaiaWeb/EcogaiaWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.EcogaiaWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcogaiaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcogaiaWebApplication.class, args);
+	}
+
+}
